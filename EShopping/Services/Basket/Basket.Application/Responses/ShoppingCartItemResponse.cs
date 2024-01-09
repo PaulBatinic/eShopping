40fcb44fae3b306bf300c761110c1da8362b9564
PaulBatinic <paul.batinic@gmail.com>
2024-01-09 13:35:39 +0100

    Shopping Cart Response Creation

--- EShopping/Services/Basket/Basket.Application/Responses/ShoppingCartItemResponse.cs
+++ EShopping/Services/Basket/Basket.Application/Responses/ShoppingCartItemResponse.cs
@@ -1,11 +1,12 @@
-namespace Basket.Core.Entities;
+namespace Basket.Application.Responses;
 
-public class ShoppingCartItem
+public class ShoppingCartItemResponse
 {
   public string ProductId { get; set; }
   public string ProductName { get; set; }
   public int Quantity { get; set; }
   public decimal Price { get; set; }
   public string ImageFile { get; set; }
+
 
 }
