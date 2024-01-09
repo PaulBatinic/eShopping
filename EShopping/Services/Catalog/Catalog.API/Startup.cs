5d171beaf5ec13583e71a6c707c174cb5ddbab7d
PaulBatinic <paul.batinic@gmail.com>
2024-01-09 03:18:06 +0100

    Catalog Docker Project Creation

--- EShopping/Services/Catalog/Catalog.API/Startup.cs
+++ EShopping/Services/Catalog/Catalog.API/Startup.cs
@@ -49,7 +49,6 @@ public class Startup
       app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
     }
 
-    app.UseHttpsRedirection();
     app.UseRouting();
     app.UseAuthentication();
     app.UseStaticFiles();
