acf2bf6279c50067c2241fefee05c764ffc573e6
PaulBatinic <paul.batinic@gmail.com>
2024-01-12 02:28:28 +0100

    Calling Discount Microservice from Basket

--- EShopping/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ EShopping/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using Basket.Application.Commands;
+using Basket.Application.GrpcService;
 using Basket.Application.Mappers;
 using Basket.Application.Queries;
 using Basket.Application.Responses;
@@ -10,13 +11,15 @@ namespace Basket.API.Controllers;
 
 public class BasketController : ApiController
 {
-    private readonly IMediator _mediator;
+  private readonly IMediator _mediator;
+  private readonly DiscountGrpcService _discountGrpcService;
 
-    public BasketController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-    
+  public BasketController(IMediator mediator, DiscountGrpcService discountGrpcService)
+  {
+      _mediator = mediator;
+      _discountGrpcService = discountGrpcService;
+  }
+
     [HttpGet]
     [Route("[action]/{userName}", Name = "GetBasketByUserName")]
     [ProducesResponseType(typeof(ShoppingCartResponse), (int) HttpStatusCode.OK)]
@@ -26,17 +29,22 @@ public class BasketController : ApiController
         var basket = await _mediator.Send(query);
         return Ok(basket);
     }
-    
+
     [HttpPost("CreateBasket")]
-    [ProducesResponseType(typeof(ShoppingCartResponse), (int) HttpStatusCode.OK)]
+    [ProducesResponseType(typeof(ShoppingCartResponse), (int)HttpStatusCode.OK)]
     public async Task<ActionResult<ShoppingCartResponse>> UpdateBasket([FromBody] CreateShoppingCartCommand createShoppingCartCommand)
     {
-        
-        var basket = await _mediator.Send(createShoppingCartCommand);
-        return Ok(basket);
-    }
     
-    [HttpDelete]
+         foreach (var item in createShoppingCartCommand.Items)
+         {
+             var coupon = await _discountGrpcService.GetDiscount(item.ProductName);
+             item.Price -= coupon.Amount;
+         }
+         var basket = await _mediator.Send(createShoppingCartCommand);
+         return Ok(basket);
+    }
+
+  [HttpDelete]
     [Route("[action]/{userName}", Name = "DeleteBasketByUserName")]
     [ProducesResponseType((int) HttpStatusCode.OK)]
     public async Task<ActionResult<ShoppingCartResponse>> DeleteBasket(string userName)
