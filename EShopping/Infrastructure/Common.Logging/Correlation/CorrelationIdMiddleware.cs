93bc8397eca1919b8d2022867c8e9d1d12a8965c
PaulBatinic <paul.batinic@gmail.com>
2024-01-25 15:31:57 +0100

    Creating Correlation Id Generator

---
+++ EShopping/Infrastructure/Common.Logging/Correlation/CorrelationIdMiddleware.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace Common.Logging.Correlation;
+
+public class CorrelationIdMiddleware
+{
+    private readonly RequestDelegate _next;
+    private const string _correlationIdHeader = "X-Correlation-Id";
+
+    public CorrelationIdMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context, ICorrelationIdGenerator correlationIdGenerator)
+    {
+        await _next(context);
+    }
+
+}
