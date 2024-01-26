656a70f71689e9c02d6f58bfee3d098d654afd49
PaulBatinic <paul.batinic@gmail.com>
2024-01-26 10:41:38 +0100

    Enriching Serilog Data

--- EShopping/Infrastructure/Common.Logging/Logging.cs
+++ EShopping/Infrastructure/Common.Logging/Logging.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
+using Serilog.Exceptions;
 using Serilog.Sinks.Elasticsearch;
 
 namespace Common.Logging;
@@ -11,7 +12,12 @@ public static class Logging
     public static Action<HostBuilderContext, LoggerConfiguration> ConfigureLogger =>
         (context, loggerConfiguration) =>
         {
+            var env = context.HostingEnvironment;
             loggerConfiguration.MinimumLevel.Information()
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("ApplicationName", env.ApplicationName)
+                .Enrich.WithProperty("EnvironmentName", env.EnvironmentName)
+                .Enrich.WithExceptionDetails()
                 .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
                 .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Information)
                 .WriteTo.Console();
